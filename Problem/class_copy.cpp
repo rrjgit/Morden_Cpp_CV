a4f4877500637d4d230c3005fff13811b606f681
rrj <rrjgit@163.com>
2018-08-08 17:06:55 +0800

    confused about class-move

---
+++ Problem/class_copy.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using std::endl; using std::cout; using std::cerr;
+
+class Hmm
+{
+ public:
+
+	Hmm(int a): num{a} { cout << "default" << endl; }
+
+	Hmm(const Hmm& other) { cout << "copy" << endl; }
+
+	Hmm(Hmm&& other) { cout << "move" << endl; }
+
+	Hmm& operator=(const Hmm& other){
+		cout << "copy operator" <<endl;
+		return *this;
+	}
+
+	Hmm& operator=(Hmm&& other){
+		cout << "move operator" << endl;
+		return *this;
+	}
+
+	int get_num() { return num; }
+
+
+ private: 
+ 	int num = 10;
+
+
+
+};
+
+int main()
+{
+	Hmm a(2);
+	cerr << a.get_num() << endl;
+
+	Hmm b = a;
+	cout << b.get_num() << endl;// Here b.get_num() is 10 not 2
+
+	a = b;
+	cout << a.get_num() << endl;// Here a.get_num() is 2
+
+	Hmm a_(3);
+	a_ = a;
+	cout << a_.get_num() << endl;
+
+	Hmm c = std::move(a);
+	cout << c.get_num() << endl;
+
+	c = std::move(b);
+
+
+
+	return 0;
+}
